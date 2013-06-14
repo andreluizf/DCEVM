2979979c162639151ac574a40a6f9c5f84559d71
sweetybanana <lepenkiny@gmail.com>
2013-06-14 17:00:41 +0400

    jre load path changed

--- src/ru/spbau/dcevm/DcevmFileManager.java
+++ src/ru/spbau/dcevm/DcevmFileManager.java
@@ -4,6 +4,8 @@ import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.util.NotNullLazyValue;
+import com.intellij.openapi.util.io.FileUtilRt;
+import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -14,7 +16,6 @@ import java.io.File;
  */
 public class DcevmFileManager {
 
-  @NotNull private static final String JRE_DIR_NAME = "jre";
   @NotNull private static final String JAVA_EXEC = "bin" + File.separator + "java";
   
   @NotNull private static final NotNullLazyValue<String> PLUGIN_HOME = new NotNullLazyValue<String>() {
@@ -30,7 +31,9 @@ public class DcevmFileManager {
   
   @NotNull
   public File getDcevmDir() {
-    return new File(PLUGIN_HOME.getValue(), JRE_DIR_NAME);
+    File home = new File(PathUtil.getParentPath(PLUGIN_HOME.getValue()), DcevmConstants.DCEVM_NAME + "_JRE");
+    FileUtilRt.createDirectory(home);
+    return home;
   }
 
   @NotNull
