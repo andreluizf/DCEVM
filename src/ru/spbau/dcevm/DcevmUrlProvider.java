4565d9164b7dc3736009d2f9e19c51c3eedce85c
Yaroslav Lepenkin <Yaroslav.Lepenkin@jetbrains.com>
2013-08-28 18:05:44 +0400

    Changes dropbox urls

--- src/ru/spbau/dcevm/DcevmUrlProvider.java
+++ src/ru/spbau/dcevm/DcevmUrlProvider.java
@@ -9,12 +9,17 @@ import org.jetbrains.annotations.Nullable;
  * Time: 7:06 PM
  */
 public class DcevmUrlProvider {
+  //private static final String DROPBOX_LINUX_32_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-linux32.zip";
+  //private static final String DROPBOX_LINUX_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-linux64.zip";
+  //private static final String DROPBOX_WIN_32_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-win32.zip";
+  //private static final String DROPBOX_WIN_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-win64.zip";
+  //private static final String DROPBOX_MACOS_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-macos64.zip";
 
-  private static final String DROPBOX_LINUX_32_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-linux32.zip";
-  private static final String DROPBOX_LINUX_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-linux64.zip";
-  private static final String DROPBOX_WIN_32_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-win32.zip";
-  private static final String DROPBOX_WIN_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-dcevm-win64.zip";
-  private static final String DROPBOX_MACOS_64_URL = "https://dl.dropboxusercontent.com/u/62224416/j2re-macos64.zip";
+  private static final String LINUX32 = "http://dcevm.nentjes.com/j2re-dcevm-linux32.zip";
+  private static final String LINUX64 = "http://dcevm.nentjes.com/j2re-dcevm-linux64.zip";
+  private static final String MACOS64 = "http://dcevm.nentjes.com/j2re-macos64.zip";
+  private static final String WIN32 = "http://dcevm.nentjes.com/j2re-dcevm-win32.zip";
+  private static final String WIN64 = "http://dcevm.nentjes.com/j2re-dcevm-win64.zip";
 
   /**
    * @return    url which points to the target assembled DCEVM to use at the current environment;
@@ -24,21 +29,21 @@ public class DcevmUrlProvider {
   public String getUrl() {
     if (SystemInfo.isLinux) {
       if (SystemInfo.is32Bit) {
-        return DROPBOX_LINUX_32_URL;
+        return LINUX32;
       }
       else if (SystemInfo.is64Bit) {
-        return DROPBOX_LINUX_64_URL;
+        return LINUX64;
       }
     }
     else if (SystemInfo.isMac) {
-      return DROPBOX_MACOS_64_URL;
+      return MACOS64;
     }
     else if (SystemInfo.isWindows) {
       if (SystemInfo.is32Bit) {
-        return DROPBOX_WIN_32_URL;
+        return WIN32;
       }
       else if (SystemInfo.is64Bit) {
-        return DROPBOX_WIN_64_URL;
+        return WIN64;
       }
     }
     return null;
