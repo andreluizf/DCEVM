af2df06a6d9e04e21faba71231e377fb00a05341
Yaroslav Lepenkin <yaroslav.lepenkin@jetbrains.com>
2015-04-24 13:08:12 +0300

    fixed compatibility issues

--- src/ru/spbau/dcevm/DcevmNetworkManager.java
+++ src/ru/spbau/dcevm/DcevmNetworkManager.java
@@ -3,15 +3,12 @@ package ru.spbau.dcevm;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.util.Consumer;
-import com.intellij.util.io.UrlConnectionUtil;
-import com.intellij.util.net.HttpConfigurable;
-import com.intellij.util.net.NetUtils;
+import com.intellij.platform.templates.github.DownloadUtil;
+import com.intellij.util.io.HttpRequests;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
-import java.net.HttpURLConnection;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Denis Zhdanov
@@ -26,108 +23,45 @@ public class DcevmNetworkManager {
     myDcevmNotificationManager = dcevmNotificationManager;
   }
 
-  /**
-   * Tries to derive size of the content referenced by the given url.
-   * <p/>
-   * Performs I/O, that's why is expected to not be called from EDT.
-   * 
-   * @param url  url which points to the target content
-   * @return     positive value which identifies size of the content referenced by the given url;
-   *             non-positive value as an indication that target content size is unknown
-   */
-  public int getSize(@NotNull String url) {
-    final Ref<Integer> result = new Ref<Integer>();
-    execute(url, new Consumer<HttpURLConnection>() {
-      @Override
-      public void consume(HttpURLConnection connection) {
-        try {
-          int code = connection.getResponseCode();
-          if (code == HttpURLConnection.HTTP_OK) {
-            result.set(connection.getContentLength());
-          } else {
-            throw new IOException("Response code is not ok, but: " + code);
-          }
-        }
-        catch (IOException e) {
-          LOG.info("Returning remote size 0: " + e.getMessage());
-          result.set(0);
-        }
-      }
-    });
-    return result.get();
-  }
-
   public void download(@NotNull final Project project,
                        @NotNull final File destination,
                        @NotNull final String url,
                        @NotNull final ProgressIndicator indicator,
                        @NotNull final Runnable callback)
   {
-    execute(url, new Consumer<HttpURLConnection>() {
-      @Override
-      public void consume(HttpURLConnection connection) {
-        final int contentLength = connection.getContentLength();
-
-        boolean ok = false;
-        InputStream in = null;
-        OutputStream out = null;
-
-        try {
-          //Surprisingly Dropbox can return instead some 5** error.
-          if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            myDcevmNotificationManager.notifyServerError(project);
-            return;
-          }
-          in = UrlConnectionUtil.getConnectionInputStreamWithException(connection, indicator);
-          indicator.setIndeterminate(contentLength <= 0);
-          out = new BufferedOutputStream(new FileOutputStream(destination, false));
-          NetUtils.copyStreamContent(indicator, in, out, contentLength);
-          ok = true;
-        }
-        catch (IOException e) {
-          LOG.warn(String.format("Unexpected I/O exception occurred during downloading DCEVM from '%s' to '%s'",
-                                 url, destination.getAbsolutePath()),
-                   e);
-        }
-        finally {
-          if (in != null) {
-            try {
-              in.close();
-            }
-            catch (IOException e) {
-              // Ignore
-            }
-          }
-          if (out != null) {
-            try {
-              out.close();
-            }
-            catch (IOException e) {
-              e.printStackTrace();
-            }
-          }
-        }
-        if (ok) {
-          callback.run();
-        }
-      }
-    });
-  }
-
-  private void execute(@NotNull String url, @NotNull Consumer<HttpURLConnection> task) {
-    HttpURLConnection connection = null;
     try {
-      connection = HttpConfigurable.getInstance().openHttpConnection(url);
-      task.consume(connection);
-      
+      DownloadUtil.downloadContentToFile(indicator, url, destination);
+      callback.run();
     }
     catch (IOException e) {
-      LOG.warn(String.format("Unexpected I/O exception occurred during attempt to work with content at '%s'", url), e);
-    }
-    finally {
-      if (connection != null) {
-        connection.disconnect();
-      }
+      LOG.debug(e);
+      myDcevmNotificationManager.notifyServerError(project);
     }
   }
+
+
+  /**
+   * Tries to derive size of the content referenced by the given url.
+   * <p/>
+   * Performs I/O, that's why is expected to not be called from EDT.
+   *
+   * @param url  url which points to the target content
+   * @return     positive value which identifies size of the content referenced by the given url;
+   *             non-positive value as an indication that target content size is unknown
+   */
+  public int getSize(@NotNull String url) {
+    try {
+      return HttpRequests.request(url).connect(new HttpRequests.RequestProcessor<Integer>() {
+        @Override
+        public Integer process(@NotNull HttpRequests.Request request) throws IOException {
+          return request.getConnection().getContentLength();
+        }
+      });
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      return -1;
+    }
+  }
+
 }
